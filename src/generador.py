2dd05190785d9efb35f569fe8aac312a082ad69a
dev-Daniel <canalesbrady@gmail.com>
2025-06-19 22:01:06 -0400

    Corrección en el codigo de archivo generador

--- src/generador.py
+++ src/generador.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def generador_contrasena(longitud):
+def generar_contrasena(longitud):
     caracteres = string.ascii_letters + string.digits + string.punctuation
-    contrasena =''.join(random.choice(caracteres) for _ in range(longitud))
+    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud))
     return contrasena
