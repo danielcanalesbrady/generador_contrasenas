b0b0d2a601944294fc5a54305856e06ce434e3f4
dev-Daniel <canalesbrady@gmail.com>
2025-06-22 19:34:54 -0400

    Se agrega función contraseñas a txt

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from generador import generar_contrasena
 
+def guardar_en_archivo(contrasena):
+    with open("contrasenas_generadas.txt", "a") as archivo:
+        archivo.write(contrasena + "\n")
+
 def iniciar():
     try:
         longitud = int(input("¿Cuántos caracteres quieres en tu contraseña?: "))
@@ -8,6 +12,10 @@ def iniciar():
             return
         contrasena = generar_contrasena(longitud)
         print("Tu contraseña segura es:", contrasena)
+
+        guardar_en_archivo(contrasena)
+        print("Contraseña guardada en 'contrasenas_generadas.txt'.")
+
     except ValueError:
         print("Por favor ingresa un número válido.")
 
